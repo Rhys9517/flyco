5c40db60c548b7e006564206d77815580e9c6422
Rhys9517 <395877670@examole.com>
2018-12-13 18:53:03 +0800

    ..

--- dist/static/js/register.js
+++ dist/static/js/register.js
@@ -40,3 +40,69 @@ $(function() {
     }
   })
 });
+// //注册唯一验证
+// var uname =$("#uname");
+// var upwd =$("#upwd");
+// var form=$("form");
+// var flag=false;
+// var flag1=false;
+
+// uname.keyup(function() {
+
+
+// if (reg.test(uname.val())) {
+//   $.get("../php/onlyunamecheck.php", { uname: uname.val() }, function(res) {
+    
+//     console.log(res)
+//    if (res == 0) {
+//      $(".uname-down").html(
+//         "你输入的会员名" + uname.val()  + "已被注册,请输入其它会员名尝试"
+//       );
+//       $(".uame-down").css("color","red");
+ 
+//       flag = false;
+//     } else {
+//       $(".uname-down").html("恭喜你! " + " 可以注册");
+   
+//       flag = true;
+//       $(".uname-down").css("color","green");
+//     }
+//   });
+// } else {
+//   $(".uname-down").html(
+//     "你输入的会员名" +
+//      uname.val()  +
+//       " 太短或太长或保护非法字符,请输入6-20位的字母和数字"
+//   );
+//   $(".uname-down").css("color", "red");
+//   flag = false;
+// }
+// if (uname.val()  == "") {
+//   $(".uname-down").html(
+//     "请使用英文和数字,不要使用下划线等特殊符号。"
+//   );
+//   $(".uname-down").css("color", "black");
+//   flag = false;
+// }
+// });
+
+// upwd.keyup(function(){
+//   var pwdgood = /^[0-9a-zA-Z]{6,20}$/
+//   if(pwdgood.test(upwd.val())){
+//     $(".password-down").html("密码可用");
+//     $(".password-down").css("color","green");
+//     flag1=true;
+//   }else{
+//     $(".password-down").html("密码不可用");
+//     $(".password-down").css("color","red");
+//     flag1=false;
+//   }
+// });
+//   form.submit(function(){
+//     if(flag && flag1){
+//       return true;
+//     }else{
+//       alert("请填写正确信息");
+//       return false;
+//     }
+//   })
